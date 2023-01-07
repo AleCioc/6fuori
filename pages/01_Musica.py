4f30139a307498e62395406dbdef87b04001fb17
alecioc <alessandro@ciociola.me>
2023-01-07 14:35:20 +0100

    iframe pdf instead of embed

--- pages/01_Musica.py
+++ pages/01_Musica.py
@@ -51,7 +51,7 @@ base64_pdf = base64.b64encode(
 ).decode('utf-8')
 
 pdf_display = F"""
-    <iframe src="data:application/pdf;base64,{base64_pdf}" width="1000" height="1000" type="application/pdf">
+    <iframe src="data:application/pdf;base64,{base64_pdf}" width="1000" height="1000" type="application/pdf"></iframe>
 """
 
 st.markdown(pdf_display, unsafe_allow_html=True)
